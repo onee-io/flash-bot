1c3b52cb656ef82b7dff081e0e4114d722a85b99
onee <wangyu@onee.io>
2022-02-24 22:27:35 +0800

    :sparkles: 临时提交

--- scripts/quick-script.js
+++ scripts/quick-script.js
@@ -28,8 +28,8 @@ async function approve(tokenAddress, spenderAddress) {
     console.log("代币授权完成");
 }
 
-// 多路由兑换
-async function executeSwap(flashBotAddress, amountIn, path, router) {
+// 多路由套利兑换
+async function executeArbitrageSwap(flashBotAddress, amountIn, path, router) {
     // 加载机器人合约
     let abi = [{
         "inputs": [
@@ -56,37 +56,37 @@ async function executeSwap(flashBotAddress, amountIn, path, router) {
             "type": "tuple"
           }
         ],
-        "name": "executeSwap",
+        "name": "executeArbitrageSwap",
         "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
       }];
     let contract = new ethers.Contract(flashBotAddress, abi, wallet);
     let param = { amountIn, path, router };
-    const tx = await contract.executeSwap(param, options);
-    console.log("多路由兑换请求已提交 待区块链确认 tx => ", tx);
+    const tx = await contract.executeArbitrageSwap(param, options);
+    console.log("多路由套利兑换请求已提交 待区块链确认 tx => ", tx);
     // 等待打包
     await tx.wait();
-    console.log("多路由兑换完成");
+    console.log("多路由套利兑换完成");
 }
 
 // 主函数
 async function main() {
     // 合约地址
-    let token0Address = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"; // USDC
-    let token1Address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"; // WMATIC
-    let quickswapRouterAddress = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
-    let sushiswapRouterAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    let token0Address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"; // WMATIC
+    let token1Address = "0x071ab2bf3cb7c51897d74cec58a47ae85d655956"; // TOKEN
+    let router0Address = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    let router1Address = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
     let flashBotAddress = "0xF0798939453241ac407012855Ee80c9b043BE822";
 
     // 授权代币
     // await approve(token0Address, flashBotAddress);
 
-    // 多路由兑换
-    let amountIn = 100;
+    // 多路由套利兑换
+    let amountIn = ethers.BigNumber.from("10000000000000000000");
     let path = [token0Address, token1Address, token0Address];
-    let router = [quickswapRouterAddress, sushiswapRouterAddress];
-    // await executeSwap(flashBotAddress, amountIn, path, router);
+    let router = [router0Address, router1Address];
+    await executeArbitrageSwap(flashBotAddress, amountIn, path, router);
 }
 
 main()
